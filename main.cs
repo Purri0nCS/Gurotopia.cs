3239a2e8ea2db4a4bac6bd1307e6fe6f3bc0b94d
Purri0nCS <LvAleksandrsLv@gmail.com>
2025-07-28 00:20:59 +0300

    for better structure moved into Main method.

--- main.cs
+++ main.cs
@@ -2,45 +2,54 @@
     @copyright gurotopia (c) 2025-07-27
 */
 
-ENet.Library.Initialize();
-
-using ENet.Host server = new();
-ENet.Address address = new() { Port = 17091 };
-
-server.Create(address, 50);
-
-Console.WriteLine("Server started on port " + address.Port);
-
-
-while (!Console.KeyAvailable)
+class Program
 {
-    bool polled = false;
-    while (!polled)
+    static void Main(string[] args)
     {
-        if (server.CheckEvents(out ENet.Event netEvent) <= 0)
+        ENet.Library.Initialize();
+        try
         {
-            if (server.Service(15, out netEvent) <= 0)
-                break;
+            using ENet.Host server = new();
+            ENet.Address address = new() { Port = 17091 };
+            server.Create(address, 50);
+            Console.WriteLine("Server started on port " + address.Port);
 
-            polled = true;
+            while (!Console.KeyAvailable)
+            {
+                bool polled = false;
+                while (!polled)
+                {
+                    if (server.CheckEvents(out ENet.Event netEvent) <= 0)
+                    {
+                        if (server.Service(15, out netEvent) <= 0)
+                            break;
+                        polled = true;
+                    }
+
+                    switch (netEvent.Type)
+                    {
+                        case ENet.EventType.Connect:
+                            Console.WriteLine($"Client connected - ID: {netEvent.Peer.ID}");
+                            break;
+                        case ENet.EventType.Disconnect:
+                            Console.WriteLine($"Client disconnected - ID: {netEvent.Peer.ID}");
+                            break;
+                        case ENet.EventType.Receive:
+                            Console.WriteLine($"Packet received from - ID: {netEvent.Peer.ID}, Data length: " + netEvent.Packet.Length);
+                            netEvent.Packet.Dispose();
+                            break;
+                    }
+                }
+            }
         }
-
-        switch (netEvent.Type)
+        catch (Exception ex)
         {
-            case ENet.EventType.Connect:
-                Console.WriteLine($"Client connected - ID: {netEvent.Peer.ID}");
-                break;
-
-            case ENet.EventType.Disconnect:
-                Console.WriteLine($"Client disconnected - ID: {netEvent.Peer.ID}");
-                break;
-
-            case ENet.EventType.Receive:
-                Console.WriteLine($"Packet received from - ID: {netEvent.Peer.ID}, Data length: " + netEvent.Packet.Length);
-                netEvent.Packet.Dispose();
-                break;
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            ENet.Library.Deinitialize();
         }
     }
 }
 
-ENet.Library.Deinitialize();
